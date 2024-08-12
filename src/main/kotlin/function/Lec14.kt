b51fb193fa7f3db998b036c020b4d974fb7c2677
ToasT1ng <jrainboww9@gmail.com>
2024-08-12 20:55:48 +0900

    Lec14 Lec15 Lec16 보완

--- src/main/kotlin/function/Lec14.kt
+++ src/main/kotlin/function/Lec14.kt
@@ -10,3 +10,13 @@ fun main() {
   val plusOne: () -> Unit = { num += 1 }
 }
 
+
+// 고차함수 1 : 파라미터로 함수를 받는 경우
+fun compute(num1: Int, num2: Int, op: (Int, Int) -> Int): Int {
+  return op(num1, num2)
+}
+
+// 고차함수 2 : 반환타입이 함수인 경우
+fun opGenerator(): (Int, Int) -> Int {
+  return { a, b -> a + b }
+}
--- src/main/kotlin/function/Lec16.kt
+++ src/main/kotlin/function/Lec16.kt
@@ -8,15 +8,29 @@ fun main() {
 
   val add2 = fun (a: Int, b: Int) = a + b
 
-  ::add
+  val add3 = ::add
 
-  //KStringFilter { it.startsWith("A") }
+  val personConstructor = ::Person
+  val personNameGetter = Person::name.getter
+  // Callable Reference => Java 와 Kotlin 방식 차이 존재함
+
+  KStringFilter { it.startsWith("A") }  // SAM (=Single Abstract Method) 생성자
+  consumeFilter({ it.startsWith("A") }) // fun1 vs. fun2 => 더 구체화된 함수로 해석됨
+  consumeFilter(Filter<String> { it.startsWith("A") }) // fun2로 해석됨
 }
 
+// fun1
 fun consumeFilter(filter: StringFilter) { }
 
+// fun2
 fun <T> consumeFilter(filter: Filter<T>) {}
 
 fun interface KStringFilter {
   fun predicate(str: String): Boolean
 }
+
+
+class Person(
+  val name: String,
+  val age: Int
+)
